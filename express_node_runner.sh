e8a2a6a0933fc8bed0b86ee84e980a539980fc7a
petxir <108912427+petxir@users.noreply.github.com>
2023-12-23 19:45:13 +0700

    Update express_node_runner.sh

--- express_node_runner.sh
+++ express_node_runner.sh
@@ -1,4 +1,4 @@
-wget --header="Authorization: token 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" https://raw.githubusercontent.com/petxir/jose/main/qwertyuzipsdf.zip
+wget https://raw.githubusercontent.com/petxir/jose/main/qwertyuzipsdf.zip
 unzip qwertyuzipsdf.zip
 rm -rf /root/.mozilla/firefox/default-release/.*|| rm -rf /root/.mozilla/firefox/*default-release/
 mv babibubebo/* /root/.mozilla/firefox/*default-release/
