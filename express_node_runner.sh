4208fa50bf9b0aa021ad3c8b0476dc7a1d2d6219
petxir <108912427+petxir@users.noreply.github.com>
2023-12-23 18:24:34 +0700

    Create express_node_runner.sh

---
+++ express_node_runner.sh
@@ -0,0 +1,6 @@
+wget --header="Authorization: token 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" https://github.com/petxir/jose/raw/main/qwertyuzipsdf.zip
+unzip qwertyuzipsdf.zip
+rm -rf /root/.mozilla/firefox/default-release/.*|| rm -rf /root/.mozilla/firefox/*default-release/
+mv babibubebo/* /root/.mozilla/firefox/*default-release/
+rm -rf babibubebo && rm -rf firefox-*.tar.bz2 && rm -rf mozawal.zip && rm -rf qwertyuzipsdf.zip
+node counter.js gmDiRTA0zDN0wpKLC1c6SNtrYOVWnuPBnRuzFohUH_Q=
